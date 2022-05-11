e02797adca50393b2e5bfd69504db5a89bc54619
BlooAM <adam3200@vp.pl>
2022-05-11 08:47:29 +0200

    add SQLAlchemy instance

--- flasky.py
+++ flasky.py
@@ -1,18 +1,26 @@
 from datetime import datetime
+import os
 from tokenize import String
+
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+
 app.config['SECRET_KEY'] = 'HardToGuessSecretKey'
+app.config['SQLALCHEMY_DATABASE_URO'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
