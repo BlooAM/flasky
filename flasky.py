92a8ff6a0a434aa9259fe4ae52acea1d77f62213
BlooAM <adam3200@vp.pl>
2022-05-19 09:05:48 +0200

    add template for mails

--- flasky.py
+++ flasky.py
@@ -4,7 +4,7 @@ from tokenize import String
 
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_mail import Mail
+from flask_mail import Mail, Message
 from flask_migrate import Migrate
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -22,8 +22,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') #TODO: set mail password in MAIL_USER environ var
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') #TODO: set mail user in MAIL_USER environ var
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') #TODO: set mail password in MAIL_PASWORD environ var
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -31,6 +33,16 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
 
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, 
+        sender=app.config['FLASKY_MAIL_SENDER'], 
+        recipients=[to]
+        )
+    msg.body = render_template(template, + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+    
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
