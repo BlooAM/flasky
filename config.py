a9262bc59f44fdfcd84e71de86ea144b0403d7c9
BlooAM <adam3200@vp.pl>
2022-05-23 08:46:48 +0200

    add basic prod-config

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    app.config['SECRET_KEY'] = 'HardToGuessSecretKey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
-    app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') #TODO: set mail user in MAIL_USER environ var
-    app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD') #TODO: set mail password in MAIL_PASWORD environ var
-    app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
-    app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
-    app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN') #TODO: set flasky admin name in FLASKY_ADMIN environ var
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'HardToGuessSecretKey')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').to_lower() in ['true', 'on', '1'] 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') #TODO: set mail user in MAIL_USER environ var
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') #TODO: set mail password in MAIL_PASWORD environ var
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN') #TODO: set flasky admin name in FLASKY_ADMIN environ var
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
