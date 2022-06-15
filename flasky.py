6f1b87273fd8dad05e34ee86a941d4f04409e981
BlooAM <adam3200@vp.pl>
2022-06-15 09:25:55 +0200

    modify app main script

--- flasky.py
+++ flasky.py
@@ -4,34 +4,9 @@ from app.models import User, Role
 from flask_migrate import Migrate
 
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-app = Flask(__name__)
-
-bootstrap = Bootstrap(app)
-moment = Moment(app)
-db = SQLAlchemy(app)
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')_
 migrate = Migrate(app, db)
-mail = Mail(app)
 
-
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
-    
-    def __repr__(self):
-        return '<Role %r>' % self.name
-
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-
-
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, USer=User, Role=Role)
