a66d3ec2b36a229c44752a622fa5e7743dac9c2e
BlooAM <adam3200@vp.pl>
2022-06-16 12:07:18 +0200

    move config to app folder

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
-from config import config
+from app.config import config
 
 
 bootstrap = Bootstrap()
