f7ff7eae031cae20b9849d481ac2fade03064982
BlooAM <adam3200@vp.pl>
2022-06-25 09:12:18 +0200

    fix typo in veryfing passwords

--- app/models.py
+++ app/models.py
@@ -28,7 +28,7 @@ class User(db.Model):
         self.password_hash = generate_password_hash(password)
 
     def verify_password(self, password):
-        return check_password_hash(self, password)
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return '<User %r>' % self.username
