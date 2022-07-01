7a30686a377588c42672a6cb6b1cb7fb4d70b89e
BlooAM <adam3200@vp.pl>
2022-07-01 11:00:09 +0200

    fix imports to auth templates

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,8 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
-
+    login_manager.init_app(app)
+    
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     from app.auth import auth as auth_blueprint
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from flask import render_template
 
 from app.auth import auth
-
+from app.auth.forms import LoginForm
 
 @auth.route('/login')
 def login():
-    return render_template('auth/login.html')
+    form = LoginForm()
+    return render_template('auth/login.html', form=form)
