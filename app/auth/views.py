462e8d41cb908fbfec85bbbdfbfbc5ecfe96f1e0
BlooAM <adam3200@vp.pl>
2022-07-02 09:13:32 +0200

    add route for user logging

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,20 @@
-from flask import render_template
+from flask import render_template, redirect, request, url_for, flash
+from flask_login import login_user
 
 from app.auth import auth
+from app.models import User
 from app.auth.forms import LoginForm
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remember_me.data)
+            next = request.arg.get('next')
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+        flash('Invalid user name or user passowrd')
     return render_template('auth/login.html', form=form)
